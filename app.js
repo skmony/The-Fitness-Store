e3d635100caaabf738ecfcecb28a083d762aba7f
Shashikant Mony <shashikantmony46@gmail.com>
2018-07-27 17:35:49 +0000

    Add external database

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ var itemRoutes      =require("./routes/items"),
     authRoutes      =require("./routes/auth"),
     indexRoutes     =require("./routes/index");
 
-mongoose.connect("mongodb://localhost/myShop");
+//mongoose.connect("mongodb://localhost/myShop");
+mongoose.connect("mongodb://skmony:Password1@ds257551.mlab.com:57551/the_fitness_store");
+
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
